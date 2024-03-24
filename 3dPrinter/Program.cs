a2a7b34174bc4c69285dbc2c234398d081f26566
haniadam99 <hani.adam@live.com>
2024-03-24 12:35:40 +0100

    Add project files.

---
+++ 3dPrinter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System; 
+
+class program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int printers = 1;
+        int days = 0;
+        int statuesprinted = 0; 
+
+        while (statuesprinted < n)
+        {
+            if ((n - statuesprinted) > printers)
+            {
+                days += 1;
+                printers += printers;
+                
+            }
+            else
+            {
+                days += 1;
+                statuesprinted += printers;
+            }
+        }
+        Console.WriteLine(days);
+    }
+}
